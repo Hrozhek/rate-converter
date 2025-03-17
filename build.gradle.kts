9e033279c21607ddb0c2bc7022aa9bfa62aa801b
hrozhek <hrozhek@gmail.com>
2025-03-17 17:34:15 +0300

    fix build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,6 @@ plugins {
     id("org.springframework.boot") version "3.1.3"
     id("io.spring.dependency-management") version "1.1.3"
     id("java")
-    kotlin("jvm") version "2.1.10"
-    kotlin("plugin.spring") version "2.1.10"
     id("application")
 }
 
@@ -17,32 +15,12 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
     compileOnly("org.projectlombok:lombok:1.18.36")
     annotationProcessor("org.projectlombok:lombok:1.18.36")
-
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-
-dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-}
-
-tasks.named<Test>("test") {
-    useJUnitPlatform()
 }
 
 application {
